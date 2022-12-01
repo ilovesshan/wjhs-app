98dbe94ed99030ce833ff2875b3222535e1a291e
wangyifeng <2665939276@qq.com>
2022-12-01 21:21:27 +0800

    refactor: 更换baseUrl

--- common_utils/lib/src/service_utils/http_helper_config.dart
+++ common_utils/lib/src/service_utils/http_helper_config.dart
@@ -1,6 +1,6 @@
 class HttpHelperConfig {
   static int selectIndex = 0;
-  static List<String> serviceList = ["https://4897033.cpolar.cn"];
+  static List<String> serviceList = ["https://4897033.cpolar.cn/"];
 
   static void checkSelectedIndex(int index) {
     selectIndex = index;
@@ -12,26 +12,17 @@ class HttpHelperConfig {
   static const int connectTimeout = 30000;
   static const int receiveTimeout = 30000;
 
-
   /// 公钥
   static String publicKey = "-----BEGIN PUBLIC KEY-----\n" +
       "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCXhZdxMwd3C6h0HJKFM9G3wWrJnQaAd29RYevgUhcqjjwnCsMeZ4YB0V762Q2NUP/IulTnbj0NPY4ZCV+dCp6zATAlJfY/2JxSE2jUO8OyvJBjP1IpzTYJg7zL+T+YNlUVqk0oyd1HSLM9K6YKMhq4mjK8UZFJejxbCfKN8OtZSwIDAQAB" +
       "\n-----END PUBLIC KEY-----";
 
-
-
   /// 需要 application/x-www-form-urlencoded 格式的请求URL
-  static List<String> needXWwwFormUrlencodedTypeList = [
-  ];
-
+  static List<String> needXWwwFormUrlencodedTypeList = [];
 
   /// 需要特殊处理 uaa格式的请求
-  static List<String> needAddUuaPrefixList = [
-  ];
-
+  static List<String> needAddUuaPrefixList = [];
 
   /// 过滤掉 不需要token的接口
-  static List<String> notNeedTokenList = [
-  ];
-
+  static List<String> notNeedTokenList = [];
 }
