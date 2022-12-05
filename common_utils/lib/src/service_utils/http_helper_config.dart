8017e537a29cff3c6dbf31178d1af4037565d78c
wangyifeng <2665939276@qq.com>
2022-12-05 11:57:45 +0800

    refactor: 更换baseurl

--- common_utils/lib/src/service_utils/http_helper_config.dart
+++ common_utils/lib/src/service_utils/http_helper_config.dart
@@ -1,6 +1,6 @@
 class HttpHelperConfig {
   static int selectIndex = 0;
-  static List<String> serviceList = ["https://4897033.cpolar.cn/"];
+  static List<String> serviceList = ["http://6d193e6e.cpolar.cn"];
 
   static void checkSelectedIndex(int index) {
     selectIndex = index;
